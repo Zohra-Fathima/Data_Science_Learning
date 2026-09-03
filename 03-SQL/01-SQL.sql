8dd10ef0758c7bfedc8fff14a5ed552de0cd41ae
zohra fathima <zohrafathima78625@gmail.com>
2026-09-03 20:53:30 +0530

    basics

--- 03-SQL/01-SQL.sql
+++ 03-SQL/01-SQL.sql
@@ -1,15 +1,39 @@
 CREATE DATABASE college;
 
-CREATE DATABASE IF NOT EXISTS instagram;
+USE college;
+
+CREATE TABLE student (
+	rollno INT,
+    name VARCHAR(30),
+    age INT
+);
+
+INSERT INTO student
+VALUES
+(101, "adam", 12),
+(102,"bob",14);
+
+SELECT * FROM student;
+
+SHOW DATABASES;
 
 USE instagram;
 
 CREATE TABLE user(
+	age INT,
+	CONSTRAINT CHECK(age >= 18),
 	id INT ,
-    age INT,
-    name VARCHAR (10) NOT NULL,
-    email INT UNIQUE,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
     followers INT DEFAULT 0,
-    following INT
-    CONSTRAINT CHECK(age>=13)
+    following INT,
+    PRIMARY KEY(id) -- makes a column unique and not null and used only for one
 );
+
+CREATE TABLE posts(
+	id INT PRIMARY KEY,
+    content VARCHAR(100),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
