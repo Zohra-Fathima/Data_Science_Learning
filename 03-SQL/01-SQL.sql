9a4b1809308e4f8d5d10e8f1f526ce20062ff507
zohra fathima <zohrafathima78625@gmail.com>
2026-09-04 16:43:48 +0530

    basics

--- 03-SQL/01-SQL.sql
+++ 03-SQL/01-SQL.sql
@@ -24,11 +24,24 @@ CREATE TABLE user(
 	CONSTRAINT CHECK(age >= 18),
 	id INT ,
     name VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
     followers INT DEFAULT 0,
     following INT,
     PRIMARY KEY(id) -- makes a column unique and not null and used only for one
 );
+
+INSERT INTO user
+(id,age,name,email,followers,following)
+VALUES
+(1,10,"adam","adam@gmail.com",198,200),
+(2,19,"muna","muna@gmail.com",500,677),
+(3,20,"shreya","shreya@gmail.com",222,122),
+(4,19,"moo","moo@gmail.com",333,344);
+
+SELECT id,name,age FROM user; -- selects the particular column
+SELECT * FROM user; -- to show all
+SELECT DISTINCT age FROM user; -- to show the distinct entries
+
 
 CREATE TABLE posts(
 	id INT PRIMARY KEY,
